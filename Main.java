663d31ba28cd35de162c743b121521a91fb9c227
k149191 <khaiairilia@gmail.com>
2025-09-10 12:02:44 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pbo.posttest1;
-
-/**
- *
- * @author NITRO V15
- */
 
 import java.util.Arrays;
 import java.util.ArrayList;
